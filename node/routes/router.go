3bc3853920b7bbea9d3ae5cbcfa045f2f876f8c4
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-30 01:51:38 -0300

    update router

--- node/routes/router.go
+++ node/routes/router.go
@@ -2,8 +2,15 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/zanz1n/downloader/node/middlewares"
+	"github.com/zanz1n/downloader/node/services"
 )
 
 func NewRouter(app *fiber.App) {
+	app.Use("/stats", middlewares.NewProtectedMiddleware())
 	app.Get("/stats", GetStats())
+
+	jwtProvider := services.NewJwtService()
+
+	app.Get("/file/:id", GetFile(jwtProvider))
 }
