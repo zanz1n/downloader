af8172ea4629673a57ec0337b7b2445825290a9e
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-05-07 14:41:54 -0300

    add graceful shutdown to fiber app

--- node/main.go
+++ node/main.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -13,6 +17,8 @@ import (
 	"github.com/zanz1n/downloader/shared/logger"
 )
 
+var endCh = make(chan os.Signal)
+
 func init() {
 	logger.Init()
 }
@@ -21,13 +27,24 @@ func main() {
 	config := services.GetConfig()
 
 	app := fiber.New(fiber.Config{
-		Prefork:           false,
-		ServerHeader:      "Fiber",
-		CaseSensitive:     true,
-		StrictRouting:     false,
-		JSONEncoder:       json.Marshal,
-		JSONDecoder:       json.Unmarshal,
-		StreamRequestBody: true,
+		Prefork:               false,
+		ServerHeader:          "Fiber",
+		CaseSensitive:         true,
+		StrictRouting:         false,
+		JSONEncoder:           json.Marshal,
+		JSONDecoder:           json.Unmarshal,
+		StreamRequestBody:     true,
+		DisableStartupMessage: true,
+	})
+
+	app.Hooks().OnListen(func() error {
+		logger.Info("Listenning on port %v with %v handlers", config.Port, app.HandlersCount())
+		return nil
+	})
+
+	app.Hooks().OnShutdown(func() error {
+		logger.Info("Shutting down ...")
+		return nil
 	})
 
 	app.Use(logger.NewFiberMiddleware())
@@ -37,15 +54,22 @@ func main() {
 
 	routes.NewRouter(app)
 
-	if config.UseSSL {
-		err := app.ListenTLS(fmt.Sprintf("0.0.0.0:%v", config.Port), config.SSLCertPath, config.SSLKeyPath)
-		if err != nil {
-			log.Fatalln(err)
+	signal.Notify(endCh, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+
+	go func() {
+		if config.UseSSL {
+			err := app.ListenTLS(fmt.Sprintf("0.0.0.0:%v", config.Port), config.SSLCertPath, config.SSLKeyPath)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		} else {
+			err := app.Listen(fmt.Sprintf("0.0.0.0:%v", config.Port))
+			if err != nil {
+				log.Fatalln(err)
+			}
 		}
-	} else {
-		err := app.Listen(fmt.Sprintf("0.0.0.0:%v", config.Port))
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
+	}()
+
+	<-endCh
+	app.ShutdownWithTimeout(3 * time.Second)
 }
