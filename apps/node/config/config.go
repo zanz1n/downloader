4c3e7d7ab6ba0b41cc6239263d28182eb79a99d2
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-26 04:40:55 -0300

    updating config wrapper

--- apps/node/config/config.go
+++ apps/node/config/config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io"
 	"os"
+	"sync"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
@@ -11,20 +12,24 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-var config *Config
-
 func GetConfig() *Config {
 	return config
 }
 
 type Config struct {
-	InstanceID string     `json:"id" yaml:"id"`
-	Key        string     `json:"key" yaml:"key"`
-	App        *ConfigApp `json:"app" yaml:"app"`
-	TCP        *ConfigTcp `json:"tcp" yaml:"tcp"`
+	// Is the id of the running node registered in the datbase
+	InstanceID string `json:"id" yaml:"id"`
+	// Can and will be change during runtime. Is the Key that will sign
+	// every restricted api request.
+	Key string     `json:"key" yaml:"key"`
+	App *ConfigApp `json:"app" yaml:"app"`
+	// This configures the tcp server that will run for streaming files in
+	// a better speed
+	TCP *ConfigTcp `json:"tcp" yaml:"tcp"`
 }
 
 type ConfigApp struct {
+	// The directory that the program will try to save the files
 	DataDir string     `json:"dataDir" yaml:"data-dir" default:"/var/downloader/data"`
 	Port    int        `json:"port" yaml:"port" default:"8080"`
 	SSL     *ConfigSSl `json:"ssl" yaml:"ssl"`
@@ -42,7 +47,12 @@ type ConfigTcp struct {
 	SSL     *ConfigSSl `json:"ssl" yaml:"ssl"`
 }
 
-var validate = validator.New()
+var (
+	validate = validator.New()
+	cfgPath  string
+	config   *Config
+	fileMu   = sync.RWMutex{}
+)
 
 func (c *Config) IsValid() error {
 	var err error
@@ -61,11 +71,11 @@ func (c *Config) IsValid() error {
 		return errors.New("config: 'tcp' prop must be provided")
 	}
 
-	if err = validateSsl(c.App.SSL); err != nil {
+	if err = validateSSL(c.App.SSL); err != nil {
 		return err
 	}
 	if c.TCP.Enabled {
-		if err = validateSsl(c.TCP.SSL); err != nil {
+		if err = validateSSL(c.TCP.SSL); err != nil {
 			return err
 		}
 	}
@@ -73,11 +83,19 @@ func (c *Config) IsValid() error {
 	return nil
 }
 
-func (c *Config) SetKey(v string) {
-	c.Key = v
+func (c *Config) GetKey() string {
+	fileMu.RLock()
+	defer fileMu.RUnlock()
+
+	return c.Key
 }
 
-func validateSsl(ssl *ConfigSSl) error {
+func (c *Config) SetKey(v string) {
+	c.Key = v
+	dumpToFile()
+}
+
+func validateSSL(ssl *ConfigSSl) error {
 	if ssl == nil {
 		return errors.New("config: 'ssl' prop must not be null")
 	}
@@ -93,7 +111,42 @@ func validateSsl(ssl *ConfigSSl) error {
 	return nil
 }
 
+func dumpToFile() error {
+	fileMu.Lock()
+	defer fileMu.Unlock()
+
+	file, err := os.OpenFile(cfgPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+
+	if err != nil {
+		return errors.New("config: failed to open config file")
+	}
+	defer file.Close()
+
+	buf, err := yaml.Marshal(config)
+
+	if err != nil {
+		return errors.New("config: failed to marshal configuration: " + err.Error())
+	}
+
+	if err := file.Truncate(0); err != nil {
+		return errors.New("config: failed to truncate config file: " + err.Error())
+	}
+
+	if _, err = file.Write(buf); err != nil {
+		return errors.New("config: failed to write config file: " + err.Error())
+	}
+
+	return nil
+}
+
+func DumpToFile() {
+	if err := dumpToFile(); err != nil {
+		logger.Error(err.Error())
+	}
+}
+
 func FromYamlFile(path string) error {
+	cfgPath = path
 	file, err := os.Open(path)
 
 	if err != nil {
