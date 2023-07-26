f5cca0c542cb6838a2cc9a628a2340d94beadfcf
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-26 02:02:49 -0300

    updating entrypoint

--- node/main.go
+++ node/main.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/zanz1n/downloader/node/config"
+	"github.com/zanz1n/downloader/node/server"
+	"github.com/zanz1n/downloader/shared/logger"
+)
+
+var (
+	configFile = flag.String("config", "/etc/downloader/config.yml", "The configuration file")
+	debug      = flag.Bool("debug", false, "If set, the app print debug logs")
+)
+
+var endCh = make(chan os.Signal)
+
+func init() {
+	flag.Parse()
+
+	if *debug {
+		logger.SetLevel("debug")
+	}
+
+	config.MustFromYamlFile(*configFile)
+}
+
 func main() {
+	srv := server.NewServer()
+
+	signal.Notify(endCh, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	go srv.MustListenAndServe(":8080")
+
+	<-endCh
+
+	srv.Shutdown()
 }
