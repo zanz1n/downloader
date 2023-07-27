7e019e8db52c95702c662d8c6e3ff2e81e14375d
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-26 21:38:59 -0300

    adding util function to read ED keypair

--- shared/auth/utils.go
+++ shared/auth/utils.go
@@ -1,5 +1,32 @@
 package auth
 
-import "github.com/zanz1n/downloader/shared/logger"
+import (
+	"crypto/ed25519"
+	"os"
+
+	"github.com/zanz1n/downloader/shared/logger"
+)
 
 var authLogger = logger.NewLogger("auth")
+
+func MustReadJwtKeyPair(privKeyFile, pubKeyFile string) (ed25519.PrivateKey, ed25519.PublicKey) {
+	jwtPriv, err := os.ReadFile(privKeyFile)
+	if err != nil {
+		authLogger.Fatal(
+			"Failed to open jwt private key file at '%s': %s",
+			privKeyFile,
+			err.Error(),
+		)
+	}
+
+	jwtPub, err := os.ReadFile(pubKeyFile)
+	if err != nil {
+		authLogger.Fatal(
+			"Failed to open jwt public key file at '%s': %s",
+			pubKeyFile,
+			err.Error(),
+		)
+	}
+
+	return jwtPriv, jwtPub
+}
