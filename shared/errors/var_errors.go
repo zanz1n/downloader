06ed52389f2b3bf79f765d91692f6b89e8d85000
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-28 00:40:40 -0300

    updating var errors

--- shared/errors/var_errors.go
+++ shared/errors/var_errors.go
@@ -1,16 +1,24 @@
 package errors
 
 var mpe = map[error]StatusError{
-	ErrPasswordHashingFailed: statusInternalServerError,
-	ErrUserAuthFailed:        statusUserAuthFailed,
-	ErrTokenGenerationFailed: statusInternalServerError,
-	ErrRouteRequiresAuth:     statusRouteRequiresAuth,
-	ErrExpiredJwtToken:       statusExpiredJwtToken,
-	ErrInvalidJwtToken:       statusInvalidJwtToken,
-	ErrDecodeTokenUnknownErr: statusInternalServerError,
-	ErrInvalidAuthHeader:     statusInvalidAuthHeader,
-	ErrFileAccessDenied:      statusFileAccessDenied,
-	ErrFileNotFound:          statusFileNotFound,
+	ErrPasswordHashingFailed:  statusInternalServerError,
+	ErrUserAuthFailed:         statusUserAuthFailed,
+	ErrTokenGenerationFailed:  statusInternalServerError,
+	ErrRouteRequiresAuth:      statusRouteRequiresAuth,
+	ErrExpiredJwtToken:        statusExpiredJwtToken,
+	ErrInvalidJwtToken:        statusInvalidJwtToken,
+	ErrDecodeTokenUnknownErr:  statusInternalServerError,
+	ErrInvalidAuthHeader:      statusInvalidAuthHeader,
+	ErrFileAccessDenied:       statusFileAccessDenied,
+	ErrFileNotFound:           statusFileNotFound,
+	ErrFileNotLocatedInNode:   statusFileNotLocatedInNode,
+	ErrInvalidEmailProvided:   statusInvalidEmailProvided,
+	ErrInvalidSignUpPayload:   statusInvalidSignUpBody,
+	ErrUserAlreadyExists:      statusUserAlreadyExists,
+	ErrEmailTooLarge:          statusEmailTooLarge,
+	ErrPasswordWeak:           statusPasswordWeak,
+	ErrUsernameTooLarge:       statusUsernameTooLarge,
+	ErrNanoIdGenerationFailed: statusInternalServerError,
 }
 
 var (
@@ -59,23 +67,60 @@ var (
 		httpCode: 404,
 		message:  "The requested file could not be found",
 	}
-	statusFIleNotLocatedInNode = &statusErrorImpl{
+	statusFileNotLocatedInNode = &statusErrorImpl{
 		code:     4043,
 		httpCode: 404,
 		message:  "The requested file is not located in this node",
 	}
+	statusInvalidEmailProvided = &statusErrorImpl{
+		code:     4001,
+		httpCode: 400,
+		message:  "The provided email address is not valid",
+	}
+	statusInvalidSignUpBody = &statusErrorImpl{
+		code:     4002,
+		httpCode: 400,
+		message:  "The provided sign up body is not valid",
+	}
+	statusUserAlreadyExists = &statusErrorImpl{
+		code:     4091,
+		httpCode: 409,
+		message:  "This user already exists, maybe try a different email address",
+	}
+	statusEmailTooLarge = &statusErrorImpl{
+		code:     4003,
+		httpCode: 400,
+		message:  "The provided email address is too big",
+	}
+	statusPasswordWeak = &statusErrorImpl{
+		code:     4004,
+		httpCode: 400,
+		message:  "The provided password can not be used, too weak",
+	}
+	statusUsernameTooLarge = &statusErrorImpl{
+		code:     4005,
+		httpCode: 400,
+		message:  "The provided username is too large to be used",
+	}
 )
 
 var (
-	ErrRouteRequiresAuth     = New("this route requires authorization")
-	ErrTokenGenerationFailed = New("failed to generate the jwt token")
-	ErrPasswordHashingFailed = New("failed to hash user password")
-	ErrUserAuthFailed        = New("user not found or password do not match")
-	ErrInvalidJwtToken       = New("the provided authorization token is invalid")
-	ErrExpiredJwtToken       = New("the provided authorization token is expired")
-	ErrDecodeTokenUnknownErr = New("something went wrong while decoding the token")
-	ErrInvalidAuthHeader     = New("the provided authorization header is not valid")
-	ErrFileAccessDenied      = New("file access denied")
-	ErrFileNotFound          = New("file could not be found")
-	ErrFileNotLocatedInNode  = New("file is not located in this node")
+	ErrRouteRequiresAuth      = New("this route requires authorization")
+	ErrTokenGenerationFailed  = New("failed to generate the jwt token")
+	ErrPasswordHashingFailed  = New("failed to hash user password")
+	ErrUserAuthFailed         = New("user not found or password do not match")
+	ErrInvalidJwtToken        = New("the provided authorization token is invalid")
+	ErrExpiredJwtToken        = New("the provided authorization token is expired")
+	ErrDecodeTokenUnknownErr  = New("something went wrong while decoding the token")
+	ErrInvalidAuthHeader      = New("the provided authorization header is not valid")
+	ErrFileAccessDenied       = New("file access denied")
+	ErrFileNotFound           = New("file could not be found")
+	ErrFileNotLocatedInNode   = New("file is not located in this node")
+	ErrInvalidEmailProvided   = New("the provided email address is not valid")
+	ErrInvalidSignUpPayload   = New("the provided sign up body is not valid")
+	ErrUserAlreadyExists      = New("user already exists")
+	ErrEmailTooLarge          = New("the provided email address is too big")
+	ErrPasswordWeak           = New("the provided password can not be used, too weak")
+	ErrUsernameTooLarge       = New("the provided username is too large to be used")
+	ErrNanoIdGenerationFailed = New("failed to generate nanoid")
 )
