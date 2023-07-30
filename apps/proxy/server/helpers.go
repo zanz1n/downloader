d4d7227f3f601d73d5839df4cf06a6309499a617
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-29 23:14:41 -0300

    adding proxy utils

---
+++ apps/proxy/server/helpers.go
@@ -0,0 +1,76 @@
+package server
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"mime"
+
+	"github.com/zanz1n/downloader/proxy/config"
+	"github.com/zanz1n/downloader/shared/errors"
+	"github.com/zanz1n/downloader/shared/logger"
+	"github.com/zanz1n/downloader/shared/utils"
+)
+
+func sanitizeFileName(name string, contentType string) string {
+	foundDot := false
+	for i := len(name); i != 0; i-- {
+		if name[i] == '.' {
+			foundDot = true
+			break
+		}
+	}
+
+	if foundDot {
+		return name
+	}
+
+	exts, err := mime.ExtensionsByType(contentType)
+
+	if err == nil && exts != nil {
+		if len(exts) > 0 {
+			ext := exts[0]
+
+			if len(ext) > 0 {
+				if ext[0] != '.' {
+					ext = "." + ext
+				}
+			}
+
+			name = name + ext
+		}
+	}
+	return name
+}
+
+func generateSignature(rnd []byte) ([]byte, error) {
+	hash := sha256.New()
+
+	if _, err := hash.Write(rnd); err != nil {
+		logger.Error("Hashing failed: " + err.Error())
+		return nil, errors.ErrHashingFailed
+	}
+	if _, err := hash.Write(utils.S2B(config.GetConfig().Key)); err != nil {
+		logger.Error("Hashing failed: " + err.Error())
+		return nil, errors.ErrHashingFailed
+	}
+
+	base64Buf := []byte{}
+
+	base64.StdEncoding.Encode(base64Buf, hash.Sum([]byte{}))
+
+	return base64Buf, nil
+}
+
+func randomString(l int) (string, error) {
+	b := make([]byte, l)
+
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	bo := []byte{}
+
+	base64.RawStdEncoding.Encode(bo, b)
+
+	return utils.B2S(bo), nil
+}
