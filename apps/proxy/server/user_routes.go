eed470e92c86d482c20b86409c5e7c2785fc29e7
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-07-30 02:15:36 -0300

    adding sign in route

--- apps/proxy/server/user_routes.go
+++ apps/proxy/server/user_routes.go
@@ -7,7 +7,36 @@ import (
 	"github.com/zanz1n/downloader/proxy/config"
 	"github.com/zanz1n/downloader/proxy/repository/user"
 	"github.com/zanz1n/downloader/shared/errors"
+	"github.com/zanz1n/downloader/shared/logger"
 )
+
+func (s *Server) HandleSignin(c *fasthttp.RequestCtx) {
+	if c.IsBodyStream() {
+		s.HandleError(c, errors.ErrStreamNotSupported)
+		return
+	}
+
+	payload := user.SignInBody{}
+	if err := json.Unmarshal(c.Request.Body(), &payload); err != nil {
+		logger.Debug(err.Error())
+		s.HandleError(c, errors.ErrInvalidJson)
+		return
+	}
+
+	if err := payload.Validate(); err != nil {
+		s.HandleError(c, err)
+		return
+	}
+
+	token, err := s.as.AuthUser(payload.Email, payload.Password)
+	if err != nil {
+		s.HandleError(c, err)
+		return
+	}
+
+	c.SetStatusCode(201)
+	respondJson(c, dataBody(token, "Success"))
+}
 
 func (s *Server) HandleSignup(c *fasthttp.RequestCtx) {
 	cfg := config.GetConfig()
