ca356c4ee153487431084f832957c7069b536f9f
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-10-04 22:22:09 -0300

    add hex encoding to binary field for better readability

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -19,5 +19,37 @@ pub struct ObjectData {
     pub name: String,
     pub mime_type: String,
     pub size: u64,
+    #[serde(with = "hex_sha256")]
     pub checksum_256: [u8; 32],
+}
+
+mod hex_sha256 {
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    #[inline]
+    pub fn serialize<S: Serializer>(
+        slice: &[u8],
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
+        hex::encode(slice).serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(
+        deserializer: D,
+    ) -> Result<[u8; 32], D::Error> {
+        let s = String::deserialize(deserializer)?;
+        hex::decode(s)
+            .map_err(|err| {
+                serde::de::Error::custom(format!(
+                    "failed to decode sha256 hex: {err}"
+                ))
+            })?
+            .try_into()
+            .map_err(|v: Vec<u8>| {
+                serde::de::Error::custom(format!(
+                    "the sha256 length is invalid: expected 32, got {}",
+                    v.len()
+                ))
+            })
+    }
 }
