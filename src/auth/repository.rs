8bfedd3b22c3f96cc75dd2bf0f04d824276a0c18
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-10-07 00:06:37 -0300

    add token repository unit tests

--- src/auth/repository.rs
+++ src/auth/repository.rs
@@ -52,14 +52,14 @@ impl TokenRepository {
     ) -> Result<String, AuthError> {
         let now = Utc::now();
 
-        let claims = UserToken {
+        let claims = Token::User(UserToken {
             user_id,
             created_at: now,
             expiration: now + self.user_token_duration,
             issuer: "SRV".into(),
             permission,
             username,
-        };
+        });
 
         jsonwebtoken::encode(&self.header, &claims, &self.enc_key)
             .map_err(|_| AuthError::GenerateTokenFailed)
@@ -81,13 +81,13 @@ impl TokenRepository {
 
         let now = Utc::now();
 
-        let claims = FileToken {
+        let claims = Token::File(FileToken {
             file_id,
             created_at: now,
             expiration: now + expiration,
             issuer,
             permission,
-        };
+        });
 
         jsonwebtoken::encode(&self.header, &claims, &self.enc_key).map_err(
             |error| {
@@ -118,5 +118,122 @@ impl TokenRepository {
 
         let eq = vec.iter().eq(&self.srv_secret);
         Ok(eq)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use base64::Engine;
+    use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey};
+    use rand::RngCore;
+    use test_log::test;
+    use uuid::Uuid;
+
+    use crate::auth::{Permission, Token};
+
+    use super::TokenRepository;
+
+    const USER_TOKEN_DURATION: Duration = Duration::from_secs(1);
+
+    fn rand_vec(size: usize) -> Vec<u8> {
+        let mut vec = vec![0u8; size];
+        rand::thread_rng().fill_bytes(&mut vec);
+        vec
+    }
+
+    fn rand_string() -> String {
+        base64::engine::general_purpose::STANDARD.encode(rand_vec(24))
+    }
+
+    fn repository() -> TokenRepository {
+        let key = rand_vec(512);
+        let srv_secret = rand_vec(128);
+
+        let algo = Algorithm::HS256;
+        let enc_key = EncodingKey::from_secret(&key);
+        let dec_key = DecodingKey::from_secret(&key);
+
+        let user_token_duration = USER_TOKEN_DURATION;
+        let max_token_duration = Duration::from_secs(30 * 24 * 3600);
+
+        TokenRepository::new(
+            algo,
+            enc_key,
+            dec_key,
+            user_token_duration,
+            max_token_duration,
+            srv_secret,
+        )
+    }
+
+    #[test]
+    fn test_create_user_token() {
+        let repo = repository();
+
+        let user_id = Uuid::new_v4();
+        let permission = Permission::empty()
+            .union(Permission::UNPRIVILEGED)
+            .union(Permission::WRITE_USERS);
+        let username = rand_string();
+
+        let tk = repo
+            .generate_user_token(user_id, permission, username.clone())
+            .unwrap();
+
+        let data = repo
+            .decode_token(&tk)
+            .expect("failed to decode generated token");
+
+        let data = match data {
+            Token::User(v) => v,
+            Token::File(_) => panic!("decoded wrong token type"),
+        };
+
+        assert_eq!(data.issuer, "SRV");
+        assert_eq!(
+            (data.expiration - data.created_at).num_seconds(),
+            USER_TOKEN_DURATION.as_secs() as i64
+        );
+        assert_eq!(data.permission, permission);
+        assert_eq!(data.user_id, user_id);
+        assert_eq!(data.username, username);
+    }
+
+    #[test]
+    fn test_create_file_token() {
+        let repo = repository();
+
+        let file_id = Uuid::new_v4();
+        let expiration = Duration::from_secs(327);
+        let issuer = format!("user/{}", Uuid::new_v4());
+        let permission = Permission::ADMIN;
+
+        let tk = repo
+            .generate_file_token(
+                file_id,
+                expiration,
+                issuer.clone(),
+                permission,
+            )
+            .unwrap();
+
+        let data = repo
+            .decode_token(&tk)
+            .expect("failed to decode generated token");
+
+        let data = match data {
+            Token::User(_) => panic!("decoded wrong token type"),
+            Token::File(v) => v,
+        };
+
+        assert_eq!(data.issuer, issuer);
+        assert_eq!(
+            (data.expiration - data.created_at).num_seconds(),
+            expiration.as_secs() as i64
+        );
+        assert_eq!(data.permission, permission);
+        assert_eq!(data.file_id, file_id);
     }
 }
