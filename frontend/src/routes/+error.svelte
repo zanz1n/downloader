1b7d8508a6ec373ffb28015a8e3a9c78830ae076
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-10-20 16:00:29 -0300

    add svelte error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { page } from "$app/stores";
+
+    let message = $page.error?.message ?? "Unknown";
+    let status = $page.status;
+</script>
+
+<svelte:head>
+    <title>{status} {message}</title>
+</svelte:head>
+
+<div class="size-full flex flex-col gap-5 items-center justify-center">
+    <h1 class="h1 text-9xl">{status}</h1>
+    <h1 class="h1">{message}</h1>
+</div>
