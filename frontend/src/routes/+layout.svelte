9d4332113f2ee403af55c8f0a2825201978c2563
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-10-25 21:51:17 -0300

    add header component to app layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
     import "../app.postcss";
+    import type { PageData } from "./$types";
 
-    // Floating UI for Popups
     import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
-    import { storePopup } from "@skeletonlabs/skeleton";
+    import { storePopup, AppShell, initializeStores } from "@skeletonlabs/skeleton";
+    import Header from "$lib/components/Header.svelte";
+
     storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+    initializeStores();
+
+    export let data: PageData;
 </script>
 
-<slot />
+<div class="w-screen h-screen">
+    <AppShell>
+        <svelte:fragment slot="header">
+            <Header auth={data.auth} />
+        </svelte:fragment>
+        <slot />
+    </AppShell>
+</div>
--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,3 +1,10 @@
+import { Authenticator } from "$lib/auth.js";
+
 export const prerender = false;
 export const csr = true;
 export const ssr = false;
+
+export async function load() {
+    const auth = await Authenticator.getInstance().getAuth();
+    return { auth };
+}
