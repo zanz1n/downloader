e970cccfdbde0c1e2e38b9e949096225d83e197a
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-12-08 03:40:50 -0300

    add account pages layout

---
+++ frontend/src/routes/account/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { TabAnchor, TabGroup } from "@skeletonlabs/skeleton";
+
+    const selected = (s: string) => $page.url.pathname === s;
+
+    let { children } = $props();
+</script>
+
+<div class="size-full flex flex-col">
+    <TabGroup justify="justify-center" class="bg-surface-100-800-token w-full">
+        <TabAnchor href="/account" selected={selected("/account")}>
+            Account
+        </TabAnchor>
+
+        <TabAnchor
+            href="/account/password"
+            selected={selected("/account/password")}
+        >
+            Change password
+        </TabAnchor>
+
+        <!-- <TabAnchor href="/account/api" selected={selected("/account/api")}>
+            API key
+        </TabAnchor> -->
+    </TabGroup>
+
+    {@render children()}
+</div>
