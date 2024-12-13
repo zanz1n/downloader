67f474e8d063aea041b6fef9489fc6a3db3d9a78
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-12-13 03:40:43 -0300

    update eslint rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -28,5 +28,17 @@ export default tseslint.config(
     },
     {
         ignores: ["build/", ".svelte-kit/", "dist/"]
+    },
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_"
+                }
+            ]
+        }
     }
 );
