bbb1d1f9176838bad485cc53b21949435e806001
Izan Rodrigues <izanrodrigues999@gmail.com>
2024-12-13 03:12:02 -0300

    add temporary placeholder components

---
+++ frontend/src/lib/components/file/FileShare.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { File } from "$lib/file";
+
+    type Props = {
+        file: File;
+    };
+
+    let { file }: Props = $props();
+</script>
+
+<h1 class="h1">Share "{file.data.name}"</h1>
